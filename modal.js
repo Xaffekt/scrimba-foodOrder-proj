a8b2d7ff681a2febd61b2732ee3c432733341056
Xavier Minnamon <xaffekt.dev@gmail.com>
2023-06-30 12:03:12 -0700

    file clean

--- modal.js
+++
@@ -1,8 +0,0 @@
-const modalEl = document.getElementById('modal')
-const formEl = document.getElementById('card-details-form')
-const submitBtn = document.getElementById('modal-submit-btn')
-
-
-export function showModal(){
-    
-}
